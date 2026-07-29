4f7389ade3587ce4e84f12f284e6f51e4d67b67c
Saarthi Basant Gupta <saarthibasantgupta31@gmail.com>
2026-07-30 00:26:59 +0530

    Time: 27 ms (36.92%), Space: 14.1 MB (17.16%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,19 +1,20 @@
 class Solution(object):
     def findErrorNums(self, nums):
-        nums.sort()
+        freq = {}
 
-        ans = []
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+
         duplicate = -1
+        missing = -1
 
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                duplicate = nums[i]
-                ans.append(duplicate)
-                break
+        for i in range(1, len(nums) + 1):
+            if freq.get(i, 0) == 2:
+                duplicate = i
+            elif freq.get(i, 0) == 0:
+                missing = i
 
-        for i in range(1, len(nums)+1):
-            if i not in nums:
-                ans.append(i)
-                break
-
-        return ans
+        return [duplicate, missing]
